e7c627d30b966dc9363fea0f08623e0c65678907
skidrow8852 <amine12solo@gmail.com>
2025-03-27 12:23:45 +0000

    Added entrance event player

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -7,7 +7,29 @@ pragma solidity ^0.8.13;
  */
 
 contract Raffle {
-    function enterRaffle() public {}
+    // Errors
+    error Raffle__InvalidEntranceFee();
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    // Events
+    event Raffle__Winner(address winner);
+    event Raffle__Entered(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__InvalidEntranceFee();
+        }
+        s_players.push(payable(msg.sender));
+        emit Raffle__Entered(msg.sender);
+    }
 
     function pickWinner() public {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
 }
