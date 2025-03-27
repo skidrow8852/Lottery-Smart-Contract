36387ab73d331752cc03587dd8bb9a15b6dfee88
skidrow8852 <amine12solo@gmail.com>
2025-03-27 11:53:09 +0000

    init

---
+++ src/Raffle.sol
@@ -0,0 +1,13 @@
+//// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+/**
+ * @title A simple Raffle contract
+ * @dev A contract for a raffle
+ */
+
+contract Raffle {
+    function enterRaffle() public {}
+
+    function pickWinner() public {}
+}
